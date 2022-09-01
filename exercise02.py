1318d94bb7b05cf77c9e73c211db89b0887237ea
sumiverma <sumistargazer@gmail.com>
2022-09-01 13:11:35 -0400

    exercise02

--- exercise02.py
+++ exercise02.py
@@ -5,3 +5,19 @@
 # The function should compute a new dict which combines the two dicts by summing the values for the common keys. 
 # Keys that are not common should be left out. Use the following code to test your function, 
 # but remember that your function should for all str->int dict inputs, not just the test here.
+
+
+
+def get_combined_dict(dict_one, dict_two):
+    #for all keys in dict one - check if in dict two
+    common_dict = {}
+    for key in dict_one:
+        if key in dict_two:
+          common_dict[key] = dict_one[key] + dict_two[key]  
+    
+    return common_dict
+
+my_dict_1 = {'a': 5, 'b': 12, 'c': 3, 'd': 9}
+my_dict_2 = {'b': 4, 'c': 9, 'd': 10, 'e': 16}
+combined_dict = get_combined_dict(my_dict_1, my_dict_2)
+print(combined_dict)
